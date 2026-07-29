ec925216a2b205b2494c0c9931cc87a54760f61c
Prerana Bhoi <preranabhoi32@gmail.com>
2026-07-29 23:12:09 +0530

    Add asyncHandler and custom ApiError utility

---
+++ backend/utills/ApiError.js
@@ -0,0 +1,8 @@
+export class APiError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+}
