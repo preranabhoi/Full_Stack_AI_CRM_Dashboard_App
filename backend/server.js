17faece378be88d8b0f69d635bac85109b10f762
Prerana Bhoi <preranabhoi32@gmail.com>
2026-08-03 20:03:34 +0530

    add authentication routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import morgan from "morgan";
 import { connectDB } from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/error.middleware.js";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 app.use(
@@ -21,6 +23,8 @@ if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
 app.get("/api/health", (req, res) =>
   res.json({ success: true, status: "ok", service: "CRM API" })
 );
+
+app.use("/api/auth", authRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
